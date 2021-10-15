6d0c01ef3210663180087ae72e457dca6f53a049
Luzimar Pereira de Oliveira <luzimar.oliveira@ndd.com.br>
2021-10-15 16:04:33 -0400

    Add GetUnique method

--- src/Controllers/WeatherForecastController.cs
+++ src/Controllers/WeatherForecastController.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -34,6 +33,20 @@ namespace DockerApi.Controllers
                 Summary = Summaries[rng.Next(Summaries.Length)]
             })
             .ToArray();
+        }
+
+        [HttpGet]
+        public WeatherForecast GetUnique()
+        {
+            var rng = new Random();
+            var unique = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            }).ToArray().FirstOrDefault();
+
+            return unique;
         }
     }
 }
